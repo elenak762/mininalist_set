326d4c9c3fb0350a6f5d3986e7e5239cf3e39511
elenak762 <elenak762@gmail.com>
2022-11-21 07:13:28 +0300

    added plus and minus buttons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,49 +1,51 @@
 import React, { useState } from "react";
 const Counter = (props) => {
-    const { value } = props;
+	const { value } = props;
 
-    const formatValue = () => {
-        return value === 0 ? "empty" : value;
-    };
-    const getBageClasses = () => {
-        let classes = "badge m-2 ";
-        classes += value === 0 ? "bg-warning" : "bg-primary";
-        return classes;
-    };
+	const formatValue = () => {
+		return value === 0 ? "empty" : value;
+	};
+	const getBageClasses = () => {
+		let classes = "badge m-2 ";
+		classes += value === 0 ? "bg-warning" : "bg-primary";
+		return classes;
+	};
 
-    const handleIncrement = () => {
-        console.log("handle increment");
-        // setValue((prevState) => prevState + 1);
-    };
-    const handleDecrement = () => {
-        console.log("handle decrement");
-        // setValue((prevState) => prevState - 1);
-    };
+	const handleIncrement = () => {
+		//console.log("handle increment");
+		// setValue((prevState) => prevState + 1);
+		props.onIncrement(props.id)
+	};
+	const handleDecrement = () => {
+		//console.log("handle decrement");
+		// setValue((prevState) => prevState - 1);
+		props.onDecrement(props.id)
+	};
 
-    return (
-        <div>
-            <span> {props.name}</span>
-            <span className={getBageClasses()}>{formatValue()}</span>
-            <button
-                className='btn btn-primary btn-sm m-2'
-                onClick={handleIncrement}
-            >
-                +
-            </button>
-            <button
-                className='btn btn-primary btn-sm m-2'
-                onClick={handleDecrement}
-            >
-                -
-            </button>
-            <button
-                className='btn btn-danger btn-sm m-2'
-                onClick={() => props.onDelete(props.id)}
-            >
-                Delete
-            </button>
-        </div>
-    );
+	return (
+		<div>
+			<span> {props.name}</span>
+			<span className={getBageClasses()}>{formatValue()}</span>
+			<button
+				className='btn btn-primary btn-sm m-2'
+				onClick={handleIncrement}
+			>
+				+
+			</button>
+			<button
+				className='btn btn-primary btn-sm m-2'
+				onClick={handleDecrement}
+			>
+				-
+			</button>
+			<button
+				className='btn btn-danger btn-sm m-2'
+				onClick={() => props.onDelete(props.id)}
+			>
+				Delete
+			</button>
+		</div>
+	);
 };
 
 export default Counter;
